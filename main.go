569c740e137cbaf2eaf5ab97bd49f7b5a9435177
somitshandilya <somit@drupaltechie.com>
2026-07-31 15:22:08 +0530

    Implement conference ticket booking system with user input and booking tracking

--- main.go
+++ main.go
@@ -3,20 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("Hello World")
-	// fmt.Println("This is a test")
-	// var anotherTest = "This is another test"
-	// fmt.Println(anotherTest)
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
 
-	const totalConferenceRoomTickets int = 100
-	var remainingConferenceRoomTickets int = 50
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 
-	fmt.Printf("Conference room tickets %v\n", totalConferenceRoomTickets)
-	fmt.Printf("Total: %v\n", totalConferenceRoomTickets)
-	fmt.Printf("Remaining: %v\n", remainingConferenceRoomTickets)
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	var userName string = "John Doe"
-	var userTickets uint = 2
-	fmt.Printf("User name %v booked %v tickets", userName, userTickets)
-	// fmt.Printf("User tickets: %v\n", userTickets)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
